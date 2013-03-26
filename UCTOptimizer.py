ea0ab6059324b5c4e1afb5d688c13e13361c1d7b
Colin Gavin <colin@willowtree-dev.net>
2013-03-25 22:23:12 -0700

    Fix bugs for first working version.

--- UCTOptimizer.py
+++ UCTOptimizer.py
@@ -57,7 +57,7 @@ class IntervalNode(object):
 		their visits count to zero?
 		"""
 		# if we have inflated all the children don't continue
-		if self.visits >= self.num_chunks:
+		if self.visits > self.num_chunks:
 			return None
 			
 		print "Visits: %d" % (self.visits, )
@@ -81,25 +81,34 @@ class IntervalNode(object):
 		"""Finds the best child representing an actino"""
 		if len(self.children) == 0:
 			return None
-			
-		if self.visits < self.num_chunks:
+		
+		if len(filter(lambda child: child is None, self.children)) > 0:
 			# we should hit each kid at least once.
 			# grab one of the unvisited children.
 			# the calling code should then call inflateNewChild
 			return None
 		else:
-			print repr([child.parent for child in self.children])
 			# if we have visited all the children, then we should simply take the one with the highest UCT score
 			return max(self.children, key=lambda child: child.uct_value)
 	
+	def most_visited_child(self):
+		children = filter(lambda child: child is not None, self.children)
+		if len(children) == 0:
+			return None
+		return max(children, key=lambda child: child.visits)
+
 	def find_best_interval(self):
 		"""Descends the tree picking the best child at every step"""
 		cur_node = self
 		
 		# just follow the best nodes down the root
 		while cur_node.has_children:
-			cur_node = cur_node.best_child()
-			
+			best_child = cur_node.most_visited_child()
+			if best_child is not None:
+				cur_node = best_child
+			else:
+				break
+
 		return cur_node.interval
 	
 	@property
@@ -117,7 +126,7 @@ class IntervalNode(object):
 		if self.children[interval_index] is not None:
 			return self.children[interval_index]
 			
-		self.children[interval_index] = IntervalNode((interval_index * self.chunk_width, (interval_index + 1) * self.chunk_width), self.uct_coeff, self.num_chunks)
+		self.children[interval_index] = IntervalNode((self.interval[0] + interval_index * self.chunk_width, self.interval[0] + (interval_index + 1) * self.chunk_width), self.uct_coeff, self.num_chunks)
 		
 		# now add the child to ourselves
 		self.add_child(self.children[interval_index], interval_index)
@@ -131,11 +140,11 @@ class IntervalNode(object):
 	@property
 	def uct_value(self):
 		""" Computes the UCT ranking for this node. Should only be called on children node (never the root)"""
-		if self.is_root:
+		if self.is_root():
 			return None
 			
 		# compute the UCT value. We bias our win rate based on our parents visits and our own visits
-		return (self.wins / self.visits) + self.uct_coeff * sqrt(log(self.parent.visits) / self.visits)
+		return (float(self.wins) / float(self.visits)) + self.uct_coeff * sqrt(log(self.parent.visits) / self.visits)
 	
 	@property
 	def has_children(self):
@@ -144,9 +153,15 @@ class IntervalNode(object):
 	@property
 	def is_leaf(self):
 		return not self.has_children
-		
+
 	def __repr__(self):
-		return '[%.5f, %.5f]' % (self.interval[0], self.interval[1])
+		def repr_indented(node, tabs):
+			children = ""
+			for child in node.children:
+				if child is not None:
+					children += tabs + repr_indented(child, tabs + '\t') + '\n'
+			return '%i/%i@<%.5f, %.5f> :\n%s' % (node.wins, node.visits, node.interval[0], node.interval[1], children)
+		return repr_indented(self, '\t')
 		
 	def propegate_result(self, result):
 		"""Propegates the UCT results back up the tree. The tuple should be the parameter, the value, and the result."""
@@ -164,7 +179,8 @@ class IntervalNode(object):
 		"""Sets the child at the specified chunk index"""
 		self.children[chunk_index] = child
 		child.parent = self
-			
+
+
 class UCTOptimizer(object):
 	"""Uses UCT to optimize the values of a number of parameters"""
 	
@@ -174,6 +190,8 @@ class UCTOptimizer(object):
 		self.uct_coeff	  = uct_coeff
 		self.num_chunks   = 10 # number of chunks on each level
 		self.horizen 	  = 20 # the depth of the tree. Ideally we should calculate this based on the desired precision
+		self.samples_loss = []
+		self.samples_win  = []
 		
 	def sample(self, node, param):
 		"""Takes a 'sample' by running a game using the parameters within this node's interval"""
@@ -191,13 +209,27 @@ class UCTOptimizer(object):
 			win_loss = subprocess.check_output(command.split(' ')).strip()
 		except subprocess.CalledProcessError as e:
 			win_loss = e.output.strip()
-			
+		
+		if win_loss == 'W':
+			self.samples_win.append(p)
+		else:
+			self.samples_loss.append(p)
+
 		print 'Sampled: %s' % (win_loss, )
 		# inform the node about the samples
 		node.propegate_result((param, p, win_loss))
 		
 		return win_loss
-		 
+
+	def output_mathematica(self, l):
+		c = "{"
+		for idx, sample in enumerate(l):
+			c += str(sample)
+			if idx != len(l) - 1:
+				c += ", "
+		c += "};"
+		return c
+
 	def tune_params(self, params):
 		
 		"""
@@ -226,25 +258,29 @@ class UCTOptimizer(object):
 			# run down the tree until we hit a node without all of it's children expanded
 			# or we hit a leaf node and want to expand the tree
 			while cur_node is not None:
-				
 				print "heading down the tree"
 				# the best child call will also tend to inflate other children at the same level
 				# it returns None if we need to inflate a child
 				parent_node = cur_node
 				cur_node = parent_node.best_child()
-				
-				
+
 			# oops! we've fallen out of the tree. Randomly add a new if we are at a leaf node with a new sample.
 			# no notion of "playouts" unless we thought about fixed tree depth
 			new_child = parent_node.inflate_random_child()
 			
 			self.sample(new_child, p1[0])
 		
+		print root
+
+		print self.output_mathematica(self.samples_win)
+		print self.output_mathematica(self.samples_loss)
+
 		# now find the best interval
-		print root.find_best_interval()
+		best_interval = root.find_best_interval()
+		return (params[0][0], best_interval[0], best_interval[1])
 		
 if __name__ == "__main__":
-	tuner = UCTOptimizer(RUN_COMMAND, 100, .3)
+	tuner = UCTOptimizer(RUN_COMMAND, 400, 0.3)
 	params = tuner.tune_params([('k', 0.0, 1000.0)])
 	
 	print params
--- test_uct_optimizer.py
+++ test_uct_optimizer.py
@@ -2,10 +2,20 @@
 # simple script to test the UCT optimizer
 
 from random import gauss, random
+import sys
 
 p = random()
 
-if p < .5:
+k = float(sys.argv[2])
+
+if k <= 500:
+    prob = -k*(k - 500.0)/(62500.0)
+else:
+    prob = -(k - 500.0)*(k - 1000.0)/(2*62500.0)
+
+#print k, prob
+
+if p < prob:
 	print 'W'
 else:
 	print 'L'
